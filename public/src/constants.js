b4294c56c2b62b70738e7eef1c727e8fb5610860
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-27 02:39:43 -0700

    Commiting a project

--- public/src/constants.js
+++
@@ -1,1 +0,0 @@
-export const ADD_REMINDER = 'ADD_REMINDER';
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,9 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <div className="title">
-           Reminder Pro
-      </div>
-      <div className="form-inline">
-     <div className="form-group">
-     <input
-     className="form-control"
-     placeholder="I have to..."
-     />
-     </div>
-     <button
-     type="button"
-     className="btn btn-success"
-     >
-     Add reminder
-     </button>
-      </div>
-      </div>
+       <div>Reminder Pro</div>
     )
   }
 }
