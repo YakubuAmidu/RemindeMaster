a10447f1023e49a5c973846de071f26d971011fb
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-28 18:28:07 -0700

    Commiting changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
+
+<Provider>
+<App />
+</Provider>,
+ document.getElementById('root'))
