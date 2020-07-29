02ddebebdd585343021d9e230b3f753d0958f5cb
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-29 01:09:20 -0700

    Commiting changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const reminder = (action) => {
  let { text, dueDate } = action;
@@ -18,10 +19,11 @@ const removeById = ( state = [], id) => {
 
 const reminders = (state = [], action) => {
   let reminders = null;
+  state = read_cookie('reminders');
   switch(action.type) {
    case ADD_REMINDER:
     reminders = [...state, reminder(action)];
-    console.log('reminders as state', reminders);
+   bake_cookie('reminders', reminders);
     return reminders;
     case DELETE_REMINDER:
      reminders = removeById(state, action.id);
