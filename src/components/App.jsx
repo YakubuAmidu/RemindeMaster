11ae94b62857c21752fd57e1fd0caf7deedb45fe
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-28 19:08:18 -0700

    Commiting changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+ constructor(props){
+   super(props);
+   this.state = {
+     text: ' '
+   }
+ }
+
   render() {
     return (
        <div className="App">
