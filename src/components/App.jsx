51bc3d9628f76f7012fb3e092ca21fbd76590acf
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-28 22:50:46 -0700

    Commiting changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,11 @@ class App extends Component {
    this.props.addReminder(this.state.text);
  }
 
+ deleteReminder(id) {
+   console.log('deleting in application', id);
+   console.log('this.props', this.props);
+ }
+
  renderReminders() {
    const { reminders } = this.props;
    return (
@@ -23,7 +28,9 @@ class App extends Component {
                return (
                  <li key={reminder.id} className="list-group-item">
                     <div className="list-item">{reminder.text}</div>
-                    <div className="list-item delete-button">
+                    <div className="list-item delete-button"
+                      onClick={() => this.deleteReminder(reminder.id)}
+                    >
                       &#x2715;
                     </div>
                  </li>
