1d47308de2c1312b003255e6b37aa7197755819e
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-28 18:38:19 -0700

    Commiting changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,16 @@
 import { ADD_REMINDER } from '../constants';
 
+const reminder = (action) => {
+  return (
+    text: action.text,
+    id: Math.random()
+  )
+}
+
 const reminders = (state = [], action) => {
   let reminders = null;
   switch(action.type) {
-
+   case ADD_REMINDER:
+    reminders = [...state, reminder(action)]
   }
 }
