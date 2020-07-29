e48947538d3812a530acc319de6f072d093ba8e5
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-28 23:32:59 -0700

    Commiting changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
-import { ADD_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
 const reminder = (action) => {
   return {
     text: action.text,
     id: Math.random()
   }
+}
+
+const removeById = ( state = [], id) => {
+  const reminders = state.filter(reminder => reminder.id !== id);
+  console.log('new reduced reminders', reminder);
+  return reminders;
 }
 
 const reminders = (state = [], action) => {
@@ -14,6 +20,9 @@ const reminders = (state = [], action) => {
     reminders = [...state, reminder(action)];
     console.log('reminders as state', reminders);
     return reminders;
+    case DELETE_REMINDER:
+     reminders = removeById(state, action.id);
+     return reminder;
     default:
      return state;
   }
