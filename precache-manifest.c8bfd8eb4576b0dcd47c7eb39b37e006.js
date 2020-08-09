78629cbc091625ac39c76c1b7643644b12f98e35
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-09 01:10:57 -0700

    Updates

---
+++ precache-manifest.c8bfd8eb4576b0dcd47c7eb39b37e006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3effbcded296dfb25da92c938fc72536",
+    "url": "/RemiderMaster/index.html"
+  },
+  {
+    "revision": "93652563b9a13ae8844a",
+    "url": "/RemiderMaster/static/css/main.e5fffe9b.chunk.css"
+  },
+  {
+    "revision": "cfe8ee72d69b828bbaa6",
+    "url": "/RemiderMaster/static/js/2.58f8cea9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RemiderMaster/static/js/2.58f8cea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93652563b9a13ae8844a",
+    "url": "/RemiderMaster/static/js/main.e4105319.chunk.js"
+  },
+  {
+    "revision": "40e4688134313dc3e2f8",
+    "url": "/RemiderMaster/static/js/runtime-main.ab399d05.js"
+  }
+]);
